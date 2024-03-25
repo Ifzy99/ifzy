f383f0f8a040e5bdf6ee9f576fb79886a9206390
Olatunbosun Ifeoluwa <olatunbosunifeoluwa@gmail.com>
2024-03-25 20:35:59 +0100

    worked on the Navbar and it's responsiveness

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,11 @@
-import { Inter } from "next/font/google";
+import { Inter, Rubik } from "next/font/google";
 import "./globals.css";
 
+
+
 const inter = Inter({ subsets: ["latin"] });
+
+const rubik = Rubik({subsets: ["latin"],})
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +15,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} ${rubik.className}`}>{children}</body>
+      
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,36 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./public/**/*.html",
+    // "./node_modules/flowbite-react/lib/**/*.js",
+    // "./pages/**/*.{ts,tsx}",
   ],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1020px",
+      xl: "1440px"
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        softBlue: 'hsl(231, 69%, 60%)',
+        softRed: 'hsl(0, 94%, 66%)',
+        grayishBlue: 'hsl(229, 8%, 60%)',
+        veryDarkBlue: 'hsl(229, 31%, 21%)',
+      },
+      fontFamily: {
+        sans: ['Rubik', 'sans-serif'],
+      },
+       
     },
   },
-  plugins: [],
+  plugins: [
+    // require("flowbite/plugin")
+  ],
 };
