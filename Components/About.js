46546ab9e0bd13296d8149ab6db8754d1d6e3c2b
Olatunbosun Ifeoluwa <olatunbosunifeoluwa@gmail.com>
2024-04-01 15:03:40 +0100

    About comp first part done

--- Components/About.js
+++ Components/About.js
@@ -1,11 +1,34 @@
-
+import Image from "next/image";
 
 const About = () => {
   return (
     <>
-    
+      <div className="flex items-center justify-center min-h-full">
+        <div className="bg-teal-50 p-2 mx-6 rounded-2xl">
+          <div className="flex flex-col md:flex-row space-y-10">
+            <div className="mx-auto md:order-2">
+              <Image
+                src="/Images/Ifeoluwa.jpg"
+                alt=""
+                width={250}
+                height={250}
+                className="rounded-full"
+              />
+            </div>
+            <div className="p-4 space-y-6 md:space-y-12 md:w-1/2">
+              <h1 className="font-serif font-bold text-3xl mt-0 md:mt-8">
+                Hello, I'm Ifeoluwa.
+              </h1>
+              <p className="my-6 tracking-wide  my-4 leading-7 md:text-left">
+                Welcome to my space. I am a fullstack web developer, who loves
+                bulding interactive and effective web applications.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- Components/Navbar.js
+++ Components/Navbar.js
@@ -14,59 +14,57 @@ const Nav = () => {
 
   return (
     <>
-      <nav className="container relative mx-auto p-6">
-            <div className="flex items-center justify-between space-x-20 my-6">
-              <Logo/>
-              <NavLinks/>
-              <MobileMenu menuOpen={menuOpen} navToggle={navToggle}/>
-            </div>
+      <nav className="container relative mx-auto p-3">
+        <div className="flex items-center justify-between space-x-20 my-4">
+          <Logo />
+          <NavLinks />
+          <MobileMenu menuOpen={menuOpen} navToggle={navToggle} />
+        </div>
 
-            {/* Mobile Menu Links*/}
+        {/* Mobile Menu Links*/}
 
-            <div
-              id="menu"
-              className={`fixed inset-0 z-20 ${
-                menuOpen ? "flex" : "hidden"
-              } flex-col items-center self-end w-full h-full m-h-screen px-6 py-1 pt-24 pb-4 tracking-widest text-white uppercase divide-y divide-gray-500 opacity-90 bg-veryDarkBlue md:hidden`}
-                >
-                      <div className="w-full py-3 text-center">
-                        <a href="#home" className="block hover:text-softRed">
-                          Home
-                        </a>
-                      </div>
+        <div
+          id="menu"
+          className={`fixed inset-0 z-20 ${
+            menuOpen ? "flex" : "hidden"
+          } flex-col items-center self-end w-full h-full m-h-screen px-6 py-1 pt-24 pb-4 tracking-widest text-white uppercase divide-y divide-gray-500 opacity-90 bg-veryDarkBlue md:hidden`}
+        >
+          <div className="w-full py-3 text-center">
+            <a href="#home" className="block hover:text-softRed">
+              Home
+            </a>
+          </div>
 
-                      <div className="w-full py-3 text-center">
-                        <a href="#about" className="block hover:text-softRed">
-                          About
-                        </a>
-                      </div>
+          <div className="w-full py-3 text-center">
+            <a href="#about" className="block hover:text-softRed">
+              About
+            </a>
+          </div>
 
-                      <div className="w-full py-3 text-center">
-                        <a href="#services" className="block hover:text-softRed">
-                          Services
-                        </a>
-                      </div>
+          <div className="w-full py-3 text-center">
+            <a href="#services" className="block hover:text-softRed">
+              Services
+            </a>
+          </div>
 
-                      <div className="w-full py-3 text-center">
-                        <a href="skills" className="block hover:text-softRed">
-                          Skills
-                        </a>
-                      </div>
+          <div className="w-full py-3 text-center">
+            <a href="skills" className="block hover:text-softRed">
+              Skills
+            </a>
+          </div>
 
-                      <div className="w-full py-3 text-center">
-                        <a href="projects" className="block hover:text-softRed">
-                          Projects
-                        </a>
-                      </div>
+          <div className="w-full py-3 text-center">
+            <a href="projects" className="block hover:text-softRed">
+              Projects
+            </a>
+          </div>
 
-                      <div className="w-full py-3 text-center">
-                        <a href="contact" className="block hover:text-softRed">
-                          Contact
-                        </a>
-                      </div>
-
-            </div>
-
+          <div className="w-full py-3 text-center">
+            <a href="contact" className="block hover:text-softRed">
+              Contact
+            </a>
+          </div>
+        </div>
       </nav>
     </>
   );
