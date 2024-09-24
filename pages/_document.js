e2dfe6c628c39e3d3d85a7a3fe57e029c91258a0
Olatunbosun Ifeoluwa <olatunbosunifeoluwa@gmail.com>
2024-09-24 09:28:38 +0100

    "Removed font imports from globals.css, added them to _document.js, and updated tailwind.config.js with minor changes to theme and variants"

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;600;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Lora:ital,wght@0,400..700;1,400..700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;600;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Lora:ital,wght@0,400..700;1,400..700&display=swap&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap&family=Cairo+Play:wght@200..1000&display=swap"
             rel="stylesheet"
           />
           <link 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./public/**/*.html",
   ],
+  darkMode: 'class',
   theme: {
     screens: {
       sm: "480px",
@@ -47,8 +47,7 @@ module.exports = {
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
         softBlue: "hsl(231, 69%, 60%)",
@@ -67,9 +66,6 @@ module.exports = {
         'navy-blue': '#0a2472', 
         'coral': '#ff6b6b',
         'coral-dark': '#ff5252',
-      },
-      fontFamily: {
-        sans: ["Roboto", "sans-serif"],
       },
     },
   },
